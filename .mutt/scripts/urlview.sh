4c955e1b0501e6a29c798cb5d237682d7cb0e9b2
Krzysztof(Chris) Kanas <k2k_chris@o2.pl>
2016-07-24 21:53:07 +0200

    Add filtering over dragonflybsd mailto

--- .mutt/scripts/urlview.sh
+++ .mutt/scripts/urlview.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # array of filters, to be moved to external file later
-url_filters=('/\/\/lists.dragonflybsd.org\/\(mailman\|pipermail\)/d' )
+url_filters=('/lists.dragonflybsd.org\/\(mailman\|pipermail\)/d' '/<mailto:commits.*@dragonflybsd.org.*>/d' )
+# url_filters=('p' )
 filtering='sed '
 for f in ${url_filters[*]}; do
-    filtering+="-e $f"
+    filtering+="-e $f "
 done
 mail_body=$(cat|$filtering)
+
 echo $mail_body|urlview
